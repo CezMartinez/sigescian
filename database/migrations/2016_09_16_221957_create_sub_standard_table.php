cefa8a1d1889456611d20d95154d36022ab2a86e
CezMartinez <nem9311@hotmail.com>
2016-09-17 07:54:41 -0600

    modificacion de migracion de sub secciones (sub standars)

--- database/migrations/2016_09_16_221957_create_sub_standard_table.php
+++ database/migrations/2016_09_16_221957_create_sub_standard_table.php
@@ -14,12 +14,12 @@ class CreateSubStandardTable extends Migration
     public function up()
     {
        Schema::create('sub_standards', function (Blueprint $table) {
-                   $table->increments('id');
-                   $table->string('section');
-                   $table->string('bookmark');
-                   $table->integer('standard_id')->unsigned();
-                   $table->foreign('standard_id')->references('id')->on('standards')->onDelete('cascade');
-               });
+           $table->increments('id');
+           $table->string('section');
+           $table->string('bookmark');
+           $table->integer('standard_id')->unsigned();
+           $table->foreign('standard_id')->references('id')->on('standards')->onDelete('cascade');
+       });
     }
 
     /**
@@ -29,6 +29,9 @@ class CreateSubStandardTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('sub_standards',function (Blueprint $table){
+            $table->dropForeign('sub_standards_standard_id_foreign');
+        });
+        Schema::dropIfExists('sub_standards');
     }
 }
