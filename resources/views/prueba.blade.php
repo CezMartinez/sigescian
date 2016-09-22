a605dc831004c4d987f40db1cca69dc1d2fd952b
SamuelZepeda <zepeda.balmore@gmail.com>
2016-09-21 22:38:18 -0600

    para kevin

--- resources/views/prueba.blade.php
+++ resources/views/prueba.blade.php
@@ -5,27 +5,20 @@
             <label for="nombre">Nombre del procedimiento</label>
             <input class="form-control" name="nombre">
         </div>
-
         <div class="form-group">
             <label  for="descripcion">Descripcion</label>
             <textarea class="form-control" name="descripcion"></textarea>
         </div>
         <h4><strong>Pasos</strong></h4>
         <hr>
-
         <div id="pasos">
-
         </div>
         <div class="form-group">
             <a class="btn btn-lg btn-success" onclick="agregar()"><span class="glyphicon glyphicon-plus"></span> Agregar</a>
-
         </div>
-
-
         <button type="submit" class="btn btn-default">Enviar</button>
     </form>
 @endsection('content')
-
 @section('scripts')
     <script>
         var stepn=0;
@@ -38,10 +31,8 @@
                     '<span class="input-group-btn"><button class="btn btn-danger" onclick="eliminar()"><span class="glyphicon glyphicon-remove"></span></button>Eliminar</span>'+'</div></div>' +
             '<hr>');
         }
-
         function eliminar() {
             $(this).closest('div.eln').fadeOut();
         }
-
     </script>
 @endsection('scripts')
