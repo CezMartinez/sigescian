9136d4aceaee3d813a53790be330748ba99fc507
khristop <kevinchristo_garcia@hotmail.com>
2016-09-23 07:29:11 +0000

    .

--- resources/views/procedures/technician/steps_form.blade.php
+++ resources/views/procedures/technician/steps_form.blade.php
@@ -6,17 +6,23 @@
     <div class="panel-body"
     <h4>Lista de pasos</h4>
     <hr>
-    <ol id="pasos">
-    </ol>
+    <div class="row">
+      <div class="col-md-8">
+        <ol id="pasos">
+        </ol>
+      </div>
+    </div>
     <hr>
     <div class="row">
+      <div class="col-md-2">
+      </div>
       <div class="col-md-4">
         <a class="btn btn-md btn-success" onclick="agregar()"><span class="glyphicon glyphicon-plus"></span> Agregar</a>
       </div>
       <div class="col-md-4">
         <a class="btn btn-md btn-info" onclick="enviar()"><span class="glyphicon glyphicon-plus"></span> Enviar</a>
       </div>
-      <div class="col-md-4">
+      <div class="col-md-2">
       </div>
     </div>
     </div>
@@ -29,6 +35,7 @@
   <script>
       var num=0;
       var csrf = $("meta[name='csrf_token']").attr('content');
+
       function agregar() {
            num+=1;
            $('#pasos').append('<li class="esk">'+
@@ -43,6 +50,20 @@
           item.remove();
       }
 
+      function validar(){
+        var i=0;
+        var flag = true;
+        var pasos = [];
+        $('input.step').each(function(){
+          if($(this).val() == "" ){
+            $(this).addClass("has-warning");
+            flag = false;
+          }
+          i+=1;
+        });
+        return flag;
+      }
+
       function enviar(proc){
         var i=0;
         var pasos = [];
@@ -50,35 +71,38 @@
           pasos[i]= $(this).val();
           i+=1;
         });
-        console.log(pasos);
-        swal({
-          title: "¿Esta seguro de guardar?",
-          text: "se estableceran los pasos",
-          type: "info",
-          showCancelButton: true,
-          confirmButtonColor: "#658DF3",
-          confirmButtonText: "Guardar",
-          closeOnConfirm: false
-        },
-         function(){
-           $.ajax({
-             type: "POST",
-             url: "#",
-             headers: {
-                 'X-CSRF-Token': csrf,
-             },
-             data:{
-               steps: pasos,
-               id_proc: proc
-             },
-             success: function(data){
-               swal("Subido con exito", "Se han registrado los pasos", "success");
-             },
-             error: function(data){
-               swal("Error", "Problemas al subir", "error");
-             }
-           })
-         }
-      )};
+        if (validar()) {
+          swal({
+            title: "¿Esta seguro de guardar?",
+            text: "se estableceran los pasos",
+            type: "info",
+            showCancelButton: true,
+            confirmButtonColor: "#658DF3",
+            confirmButtonText: "Guardar",
+            closeOnConfirm: false
+          },
+           function(){
+             $.ajax({
+               type: "POST",
+               url: "#",
+               headers: {
+                   'X-CSRF-Token': csrf,
+               },
+               data:{
+                 steps: pasos,
+                 id_proc: proc
+               },
+               success: function(data){
+                 swal("Subido con exito", "Se han registrado los pasos", "success");
+               },
+               error: function(data){
+                 swal("Error", "Problemas al subir", "error");
+               }
+             })
+          }
+         )
+       }else {
+         swal("Error", "No pueden haber pasos vacios", "error");
+       }};
   </script>
 @endsection
