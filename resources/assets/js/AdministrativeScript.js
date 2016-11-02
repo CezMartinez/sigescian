b17db5d8f3c61644808c50373df3520980c0b454
CezMartinez <nem9311@hotmail.com>
2016-11-02 09:52:34 -0600

    arreglado archivo javascrip para agregar documentos

--- resources/assets/js/AdministrativeScript.js
+++ resources/assets/js/AdministrativeScript.js
@@ -6,80 +6,32 @@ var url;
 var procedureId = id_administrative;
 var radioValue;
 Dropzone.options.uploadFile= {
-        maxFilesize: 2, // MB
-        parallelUploads: 1,
-        acceptedFiles: ".pdf,.docx",
-        init: function(){
-            this.on("addedfile", function() {
-                radioValue =  $("input[name='type']:checked"). val();
-                if(radioValue === undefined){
-                    swal("Error",'Debe Seleccionar un tipo de archivo',"error");
-                    this.removeAllFiles(true);
-                };
-                if(radioValue==1){
-                    url = "/archivos/procedimientos/formatos/"+procedureId+'/1';
-                }
-                if(radioValue==2){
-                    url = "/archivos/procedimientos/flujograma/"+procedureId;
-                }
-                if(radioValue==3){
-                    url = "/archivos/procedimientos/anexos/"+procedureId+'/1';
-                }
-                if(radioValue==4){
-                    url = "/archivos/procedimientos/procedimiento/"+procedureId+'/1';
-                }
-                $('#type_hidden').val(radioValue);
-                // Create the remove button
-            }).ontimeout;
-            this.on("success", function(file, responseText){
-                peticiondecosas()
-                var self = this;
-                setTimeout(function(){
-                    self.removeFile(file);
-                },3500);
-            });
-        }
-    }
-
-    function deleteFile(nameFile,idProcedure,idAnnexedFile,tipo, url){
-        var csrf = $("meta[name='csrf_token']").attr('content');
-
-        swal({
-                title: "¿Esta seguro de eliminar "+nameFile+"?",
-                text: "Esta accion no puede ser revertida",
-                type: "warning",
-                showCancelButton: true,
-                confirmButtonColor: "#3c3cf2",
-                confirmButtonText: "Eliminar",
-                cancelButtonText: "Cancelar",
-                closeOnConfirm:false,
-                closeOnCancel:false,
-            },
-            function(isConfirm){
-                if (isConfirm) {
-                    $.ajax({
-                            type:'DELETE',
-                            url:url+idProcedure+'/'+idAnnexedFile+'/1',
-                            headers: {
-                                'X-CSRF-Token': csrf,
-                            },
-                            success: function(data){
-
-                            },
-                        })
-                        .done(function(data){
-                            $('#file-'+''+tipo+'-'+idAnnexedFile).fadeOut();
-                            swal("Eliminado",data,"success");
-                        })
-                        .error(function(data){
-                            swal("Error",data.responseText,"error");
-                        });
-                }else {
-                    swal("Cancelado","El registro no ha sido modificado.","error");
-                }
-            });
-
-        function peticiondecosas(){
+    maxFilesize: 2, // MB
+    parallelUploads: 1,
+    acceptedFiles: ".pdf,.docx",
+    init: function(){
+        this.on("addedfile", function() {
+            radioValue =  $("input[name='type']:checked"). val();
+            if(radioValue === undefined){
+                swal("Error",'Debe Seleccionar un tipo de archivo',"error");
+                this.removeAllFiles(true);
+            };
+            if(radioValue==1){
+                url = "/archivos/procedimientos/formatos/"+procedureId+'/1';
+            }
+            if(radioValue==2){
+                url = "/archivos/procedimientos/flujograma/"+procedureId;
+            }
+            if(radioValue==3){
+                url = "/archivos/procedimientos/anexos/"+procedureId+'/1';
+            }
+            if(radioValue==4){
+                url = "/archivos/procedimientos/procedimiento/"+procedureId+'/1';
+            }
+            $('#type_hidden').val(radioValue);
+            // Create the remove button
+        }).ontimeout;
+        this.on("success", function(file, responseText){
             $.ajax({
                     type:'GET',
                     url:url,
@@ -89,7 +41,20 @@ Dropzone.options.uploadFile= {
                 .done(function(data){
                     var radioValue = $("input[name='type']:checked"). val();
                     if(radioValue==1){
-                        agregaranexos();
+                        listaArchivos=[];
+                        listaArchivos.push('<div class="lista-formatos">')
+                        $.each(data, function(i, item){
+                            listaArchivos.push('<li id="file-formato-'+item.id+'" class="list-group-item list-group-item-info">'+
+                                '<a target="_blank" href="/archivos/procedimientos/1/1/'+item.originalName+'">'+
+                                item.title+'</a>'+
+                                '<i class="fa fa-times pull-right" onclick="deleteFile(\''+
+                                item.originalName+'\',\''+id_administrative+'\',\''+item.id+'\''+
+                                ',\'formato\',\'/procedimiento/archivos/formato/\')"></i></li>')
+                        });
+                        listaArchivos.push('</div>')
+
+                        $('div.lista-formatos').replaceWith(listaArchivos.join(''));
+                        swal("Agregado",'El formato fue agregado con exito',"success");
                     }
                     if(radioValue==2){
                         listaFlujogramas=[];
@@ -158,32 +123,49 @@ Dropzone.options.uploadFile= {
                     console.log(data);
                     swal("Error",data.responseText,"error");
                 });
-        }
-
-        function agregaranexos() {
-            listaArchivos=[];
-            listaArchivos.push('<div class="lista-formatos">')
-            $.each(data, function(i, item){
-                listaArchivos.push('<li id="file-formato-'+item.id+'" class="list-group-item list-group-item-info">'+
-                    '<a target="_blank" href="/archivos/procedimientos/1/1/'+item.originalName+'">'+
-                    item.title+'</a>'+
-                    '<i class="fa fa-times pull-right" onclick="deleteFile(\''+
-                    item.originalName+'\',\''+id_administrative+'\',\''+item.id+'\''+
-                    ',\'formato\',\'/procedimiento/archivos/formato/\')"></i></li>')
-            });
-            listaArchivos.push('</div>')
-            $('div.lista-formatos').replaceWith(listaArchivos.join(''));
-            swal("Agregado",'El formato fue agregado con exito',"success");
-        }
-
-        function agregarAsociacion(){
-            $.ajax({
-                url: "asldamsd",
-                success: function () {
-
-                }
-            })
-
-
-        }
+            var self = this;
+            setTimeout(function(){
+                self.removeFile(file);
+            },3500);
+        });
     }
+}
+
+function deleteFile(nameFile,idProcedure,idAnnexedFile,tipo, url){
+    var csrf = $("meta[name='csrf_token']").attr('content');
+
+    swal({
+            title: "¿Esta seguro de eliminar "+nameFile+"?",
+            text: "Esta accion no puede ser revertida",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3c3cf2",
+            confirmButtonText: "Eliminar",
+            cancelButtonText: "Cancelar",
+            closeOnConfirm:false,
+            closeOnCancel:false,
+        },
+        function(isConfirm){
+            if (isConfirm) {
+                $.ajax({
+                        type:'DELETE',
+                        url:url+idProcedure+'/'+idAnnexedFile+'/1',
+                        headers: {
+                            'X-CSRF-Token': csrf,
+                        },
+                        success: function(data){
+
+                        },
+                    })
+                    .done(function(data){
+                        $('#file-'+''+tipo+'-'+idAnnexedFile).fadeOut();
+                        swal("Eliminado",data,"success");
+                    })
+                    .error(function(data){
+                        swal("Error",data.responseText,"error");
+                    });
+            }else {
+                swal("Cancelado","El registro no ha sido modificado.","error");
+            }
+        });
+}
