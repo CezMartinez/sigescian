2c8aa1c9d7d473dfa5fe0f0ff339ecde31382897
CezMartinez <nem9311@hotmail.com>
2016-11-01 23:02:01 -0600

    Inicio de vista de documentos (50%) con migracion de archivos anexos por procedimiento

---
+++ database/migrations/2016_11_01_200213_create_administrative_procedure_annexed_file_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdministrativeProcedureAnnexedFileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('administrative_procedure_annexed_file', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('administrative_procedure_id')->unsigned();
+            $table->integer('annexed_file_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('administrative_procedure_annexed_file');
+    }
+}
